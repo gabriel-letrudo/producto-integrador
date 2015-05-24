ba1cde8f15330436f3b90ec30245cc1ed4f28a01
gabriel-letrudo <gabrielpg_0925@hotmail.com>
2015-05-23 19:59:36 -0500

    Update main.c

--- main.c
+++ main.c
@@ -1,14 +1,25 @@
+/*
+*       Daniel Rodríguez - Gabriel Pérez Guerrero - @ 21/Mayo/2015 // Actualizado: 23/Mayo/2015
+*
+* Diseño e implementación de la solución para el almacenamiento de un acervo de libros para una biblioteca.
+* Implementación de una estructura Pila y una estructura para la información de los libros.
+*
+* Producto_Integrador_-_Libros.c
+*
+*/
+
 # include <stdio.h>
 # include <stdlib.h>
 # include <string.h>
 # include <conio.h>
 
-# define CAD_MAX    50
-# define MAX        5
-# define SMALL      10
+# define CAD_MAX    50  //Tamaño máximo de una cadena
+# define MAX        5   //Capacidad máxima de la memoria - tamaño máximo de libros a guardar
+# define SMALL      10  //Tamaño mínimo de una cadena
 # define TRUE       1
 # define FALSE      0
 
+//Creamos una estructura con los campos para los datos del libro
 struct infoLibro{
     char titulo         [CAD_MAX];
     char autor          [CAD_MAX];
@@ -17,19 +28,25 @@ struct infoLibro{
     char numPags        [SMALL];
 };
 
+//Definimos una variable del tipo de estructura infoLibro, con el identificador tipoLibro
 typedef struct infoLibro tipoLibro;
 
+//Creamos una estructura de tipo _tipoPila para controlar el almacenamiento de libros
 struct _tipoPila{
     int tope;
     struct infoLibro tipoLibro  [MAX];
 };
 
+//Definimos una variable del tipo de estructura _tipoLibro, con el identificador tipoPila
+//Ojo: la estructura es _tipoPila, mientras que tipoPila es el identificador de la variable de ese tipo de estructura
 typedef struct _tipoPila tipoPila;
 
+//Inicializamos la pila
 void inicializa(tipoPila *pPila){
     pPila->tope == -1;
 }
 
+//Verificamos si está vacía
 int vacia(tipoPila *pPila){
     if(pPila->tope == -1){
         return TRUE;
@@ -39,6 +56,7 @@ int vacia(tipoPila *pPila){
     }
 }
 
+//Verificamos si está llena
 int llena(tipoPila *pPila){
     if(pPila->tope == MAX){
         printf("Memoria llena\n");
@@ -59,13 +77,18 @@ void pilaTope(tipoPila *pPila){
     }
 }
 */
+
+//Inserta un elemento (libro) nuevo a la pila
 void push(tipoPila *pPila, tipoLibro *pLibro){
+    //Si la pila NO está llena
     if(!llena(pPila)){
         pPila->tipoLibro[pPila->tope++] = *pLibro;
     }
 }
 
+//Remueve un elemento (libro) de la pila
 tipoLibro pop(tipoPila *pPila){
+    //Si la pila NO está vacía
     if(!vacia(pPila)){
         return(pPila->tipoLibro[--pPila->tope]);
     }
@@ -74,8 +97,10 @@ tipoLibro pop(tipoPila *pPila){
     }
 }
 
+//Prototipo de función que muestra el menú de opciones
 void muestraMenu(void);
 
+//Muestra los libros apilados
 void muestraLibros(tipoPila *pPila){
     tipoLibro *pLibroActual;
     int i;
@@ -94,8 +119,8 @@ void muestraLibros(tipoPila *pPila){
     }
 }
 
+//Función principal
 int main(void){
-
 
     struct infoLibro nuevoLibro;
     struct _tipoPila nuevaPila = {0};
@@ -112,10 +137,12 @@ int main(void){
 
     inicializa(&nuevaPila);
 
+    //Esta línea es para verificar si se están almacenando los libros en la pila
     printf("%d\n\n",nuevaPila.tope);
 
     muestraMenu();
     op=getchar();
+    //fflush(stdin) para evitar cualquier bug con carácteres ingresados
     fflush(stdin);
 
     switch(op){
@@ -125,6 +152,7 @@ int main(void){
             printf("Ingrese el titulo del libro: ");
             scanf("%[^\n]",tituloLibro);
 
+            //Copiamos tituloLibro en nuevoLibro.titulo
             strcpy(nuevoLibro.titulo,tituloLibro);
 
             fflush(stdin);
@@ -132,6 +160,7 @@ int main(void){
             printf("Ingrese la clave del Libro: ");
             scanf("%[^\n]",claveLibro);
 
+            //Copiamos claveLibro en nuevoLibro.clave
             strcpy(nuevoLibro.clave,claveLibro);
 
             fflush(stdin);
@@ -139,6 +168,7 @@ int main(void){
             printf("Ingrese el nombre completo del Autor: ");
             scanf("%[^\n]",autorLibro);
 
+            //Copiamos autorLibro en nuevoLibro.autor
             strcpy(nuevoLibro.autor,autorLibro);
 
             fflush(stdin);
@@ -146,6 +176,7 @@ int main(void){
             printf("Ingrese el nombre de la Editorial: ");
             scanf("%[^\n]",editorialLibro);
 
+            //Copiamos editorialLibro en nuevoLibro.editorial
             strcpy(nuevoLibro.editorial,editorialLibro);
 
             fflush(stdin);
@@ -153,10 +184,12 @@ int main(void){
             printf("Ingrese el numero de paginas: ");
             scanf("%s",pagsLibro);
 
+            //Copiamos pagsLibro en nuevoLibro.numPags
             strcpy(nuevoLibro.numPags,pagsLibro);
 
             fflush(stdin);
 
+            //Insertamos los datos anteriores en la pila
             push(&nuevaPila,&nuevoLibro);
         }
         goto inicio;
@@ -174,14 +207,17 @@ int main(void){
         break;
     case '3':
         system("cls");
-        muestraLibros(&nuevaPila);
+        //Esta línea es para verificar si se están almacenando los datos en la pila por cada libro
+        //muestraLibros(&nuevaPila);
         getch();
         goto inicio;
         break;
     case '4':
+        //TERMINA EXITOSAMENTE EL PROGRAMA
         EXIT_SUCCESS;
         break;
     default:
+        //En caso de ingresar cualquier carácter inválido
         printf("Opcion invalida\n");
         getch();
         goto inicio;
